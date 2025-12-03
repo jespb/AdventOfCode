b09ceb15122a7962eda0d9787369e7a963e272bd
jebatista <batista-sk@hotmail.com>
2025-12-03 14:31:36 +0900

    day 3: **

--- 2025/day3/main.py
+++ 2025/day3/main.py
@@ -1,2 +1,29 @@
 
 filenames = ["input1.txt", "input2.txt"]
+
+
+def searchForBatteryN(banks, n):
+	acc = 0
+	for bank in banks:
+		bank = [int(x) for x in bank.strip()]
+		bank_acc = 0
+		index = 0
+		for i in range(n-1,-1,-1):
+			digit = max(bank[:-i] if i!=0 else bank)
+			index = bank.index(digit)
+			bank_acc += digit*(10**i)
+			bank = bank[index+1:]
+		acc +=  bank_acc
+	print(acc)
+
+for f in filenames[:]:
+
+	# Part 1
+	n = 2
+	banks = open(f)
+	searchForBatteryN(banks, n)
+
+	# Part 2
+	n = 12
+	banks = open(f)
+	searchForBatteryN(banks, n)
