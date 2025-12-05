66a72c43f65c48c507169b7e226a853bf066fd85
jebatista <batista-sk@hotmail.com>
2025-12-05 14:23:45 +0900

    day 5: **

--- 2025/day5/main.py
+++ 2025/day5/main.py
@@ -30,3 +30,38 @@ for filename in filenames:
 	print(acc) 
 
 
+	# Part 2
+	f = [s.strip() for s in open(filename)]
+	lists = []
+	s = f[0]
+	i = 0
+	while s != "":
+		s = [int(a) for a in s.split("-")]
+		lists.append(s)
+		i+=1
+		s=f[i]
+
+	old_n_list = -1
+	n_list = len(lists)
+	while n_list != old_n_list:
+		for i in range(len(lists)):
+			l = lists.pop(0)
+			merged = False
+			j = 0
+			while j < len(lists) and not merged:
+				if l[0] >= lists[j][0] and l[0] <= lists[j][1]:
+					lists[j][1] = max(lists[j][1], l[1])
+					merged = True
+				if l[1] >= lists[j][0] and l[1] <= lists[j][1]:
+					lists[j][0] = min(lists[j][0], l[0])
+					merged = True
+				j += 1
+			if not merged:
+				lists.append(l)
+		old_n_list = n_list
+		n_list = len(lists)
+	acc = 0
+	for l in lists:
+		acc += l[1]-l[0]+1
+	print(acc)
+
