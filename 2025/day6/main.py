d2780e71af89bd20d14ffa631409f30e152004e9
João Batista <jesp.batista@gmail.com>
2025-12-07 12:41:42 +0900

    day 6: *

--- 2025/day6/main.py
+++ 2025/day6/main.py
@@ -1,2 +1,25 @@
 
 filenames = ["input1.txt", "input2.txt"]
+
+
+for filename in filenames:
+
+    # Part 1
+    f = [s.strip().replace("  "," ").replace("  "," ").replace("  "," ").split(" ") for s in open(filename)]
+    op = f[-1]
+    vals = [[int(x) for x in line] for line in f[:-1]]
+    
+    acc = 0
+    for c in range(len(op)):
+        if op[c] == "+":
+            tmp = 0
+            for r in range(len(vals)):
+                tmp += vals[r][c]
+        if op[c] == "*":
+            tmp = 1
+            for r in range(len(vals)):
+                tmp *= vals[r][c]
+        #print("--%s--"%op)
+        acc += tmp
+    print(acc)
+
