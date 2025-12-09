af6f36c913ef056f759ed9e6447ef342bfe7c1e0
jebatista <batista-sk@hotmail.com>
2025-12-09 14:32:31 +0900

    day 9: *

--- 2025/day9/main.py
+++ 2025/day9/main.py
@@ -1,2 +1,35 @@
 
 filenames = ["input1.txt", "input2.txt"]
+
+
+# part 1
+for filename in filenames:
+
+	m = [ [int(x) for x in line.strip().split(",")] for line in open(filename)]
+
+	pwd = []
+	for p1 in m:
+		pwd.append( [ (abs(p1[0]-p2[0])+1) * (abs(p1[1]-p2[1])+1)  for p2 in m] )
+
+	ma = 0
+	for line in pwd:
+		ma =max(ma, max(line))
+
+	print(ma)
+
+
+
+# part 2
+for filename in filenames:
+
+	m = [ [int(x) for x in line.strip().split(",")] for line in open(filename)]
+
+	pwd = []
+	for p1 in m:
+		pwd.append( [ (abs(p1[0]-p2[0])+1) * (abs(p1[1]-p2[1])+1)  for p2 in m] )
+
+	ma = 0
+	for line in pwd:
+		ma =max(ma, max(line))
+
+	print(ma)
