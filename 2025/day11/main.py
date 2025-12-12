5d55f9ba332d5349e90847ec0fc23a5b67b66d74
jebatista <batista-sk@hotmail.com>
2025-12-12 14:01:31 +0900

    day 11: *

--- 2025/day11/main.py
+++ 2025/day11/main.py
@@ -1,26 +1,35 @@
 
 filenames = ["input1.txt", "input2.txt"]
 
-def filldic(dic, dic_dist, curr="you"):
-	if curr in dic_dist.keys():
-		return dic_dist[curr]
-	else:
-		acc = 0
-		for k in dic[curr]:
-			tmp = filldic(dic, dic_dist, k)
-			dic_dist[k] = tmp
-			acc += tmp
 
-for filename in filenames[:1]:
+
+def no_paths(dic, from_="you", to_="out"):
+	dic_dist = {}
+	for key in dic.keys():
+		if to_ in dic[key]:
+			dic_dist[key] = 1
+
+	old = ""
+	tmp = str(dic_dist)
+	while tmp != old:
+		for key in dic.keys():
+			ts = dic[key]
+			all_found = True
+			for k in ts:
+				if not k in dic_dist.keys():
+					all_found = False
+			if all_found:
+				dic_dist[key] = sum(dic_dist[k] for k in ts)
+		old = tmp 
+		tmp = str(dic_dist)
+	return dic_dist[from_]
+
+
+for filename in filenames[:]:
 	dic = {}
 	for line in open(filename):
 		line = line.strip().split(" ")
 		dic[line[0][:-1]] = line[1:]
-	print(dic)
 
-	dic_dist = {}
-	for key in dic.keys():
-		if "out" in dic[key]:
-			dic_dist[key] = 1
-
-	print(dic_dist)
+	# part 1
+	print(no_paths(dic, "you", "out"))
